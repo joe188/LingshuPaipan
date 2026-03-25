ea1307c7ca0d3c4654f4d13e4e6be23811bdc271
joel <joel@joeldeMacBook-Air.local>
2026-03-25 12:37:02 +0800

    fix: resolve Kotlin compilation errors - remove Flipper, simplify MainApplication, fix component name

--- android/app/src/main/java/com/divination/bazi/MainActivity.kt
+++ android/app/src/main/java/com/divination/bazi/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "DivinationApp"
+  override fun getMainComponentName(): String = "灵虾排盘"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
